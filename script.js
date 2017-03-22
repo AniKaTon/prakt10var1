e393bf1e0b0f79e92d838c3af2b74e4a9ea5e362
AniKaTon <braintosha@yandex.ru>
2017-03-22 23:07:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-window.onload = function(){
-var audio = new Audio();
+let audio = new Audio();
+let start=false;
 [].forEach.call(document.querySelectorAll('.play'), function(item) {
         item.addEventListener('click', function() {
-        var path = 'http://zf.fm/download/';
-        var audioName = item.querySelector('audio').getAttribute('src');
-        audio.pause();
-        var fullPath = path+audioName;
-        audio.src = fullPath;
-        audio.play();
+      		let audioName = item.querySelector('audio').getAttribute('src');
+        	audio.src = audioName;
+       		if (!start){
+				audio.play(); start=true;
+			} else {
+        		audio.pause(); start=false;
+        	}
+
         });
     });
-}
+
+
+
+
